29748f1498864f568017284474c62ce132bc90f6
dormin <williamsben91@gmail.com>
2019-11-15 19:06:08 -0700

    removed buttons and files

--- react-google-login/demo/app.js
+++ react-google-login/demo/app.js
@@ -1,3 +1,4 @@
+/* eslint-disable consistent-return */
 import React, { useState } from 'react'
 import { GoogleLogout, GoogleLogin } from '../src/index'
 // import GoogleLogin, { GoogleLogout } from '../dist/google-login'
@@ -33,13 +34,9 @@ const MountTest = () => {
         }}
         onFailure={error}
         clientId={clientId}
-      >
-        Auth then Hide button
-      </GoogleLogin>
+      />
     )
   }
-
-  return <button onClick={() => toggleShow(true)}>show button</button>
 }
 
 export default () => (
@@ -62,39 +59,7 @@ export default () => (
       // prompt="consent"
       // className='button'
       // style={{ color: 'red' }}
-    >
-      <span>Analytics</span>
-    </GoogleLogin>
-    <br />
-    <br />
-    <GoogleLogin
-      clientId={clientId}
-      scope="https://www.googleapis.com/auth/adwords"
-      onSuccess={success}
-      onFailure={error}
-      onRequest={loading}
-      approvalPrompt="force"
-      responseType="code"
-      // uxMode="redirect"
-      // redirectUri="http://google.com"
-      // disabled
-      // prompt="consent"
-      // className='button'
-      // style={{ color: 'red' }}
-    >
-      <span>Adwords</span>
-    </GoogleLogin>
-    <br />
-    <br />
-    <GoogleLogin onSuccess={success} onFailure={error} clientId={clientId} />
-    <br />
-    <br />
-    <GoogleLogin theme="dark" onSuccess={success} onFailure={error} clientId={clientId} />
-    <br />
-    <br />
-    <GoogleLogin theme="dark" style={{ background: 'blue' }} onSuccess={success} onFailure={error} clientId={clientId} />
-    <br />
-    <br />
+    />
     <GoogleLogout buttonText="Logout" onLogoutSuccess={logout} />
   </div>
 )
