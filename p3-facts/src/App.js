6457c90fec0963e449e634fd56ff6ad2dedf0ab4
BarbieDoLLxo <barbie.slumerican@gmail.com>
2019-11-19 21:18:23 -0700

    styling

--- p3-facts/src/App.js
+++ p3-facts/src/App.js
@@ -1,19 +1,28 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import router from './Routes'
-import { Container, Row, Col} from "reactstrap"; 
-import DataCard from './components/DataCard/DataCard';
-import NavBarMain from './components/NavBar/Navbar';
-import ScrapeButton from './components/ScrapeButton/ScrapeButton';
-import './App.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import router from "./Routes";
+import { Container, Row, Col, Jumbotron, Card, Button } from "reactstrap";
+import DataCard from "./components/DataCard/DataCard";
+import NavBarMain from "./components/NavBar/Navbar";
+import ScrapeButton from "./components/ScrapeButton/ScrapeButton";
+import "./App.css";
 
 function App() {
   return (
-    <Container>
-    <h1>NavBar</h1>
-    <ScrapeButton />
-    <DataCard />
-    </Container>
+    <>
+    <div style={{ width: 300, height: 300, background: '#444'}}>    I am a square box with black background
+    </div>
+<Jumbotron>
+  <h1>Hello, world!</h1>
+  <p>
+    This is a simple hero unit, a simple jumbotron-style component for calling
+    extra attention to featured content or information.
+  </p></Jumbotron> 
+  
+  
+
+
+  </>
   );
 }
 
--- p3-facts/src/Routes.js
+++ p3-facts/src/Routes.js
@@ -1,10 +1,10 @@
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
- // res.render('index', { title: 'Express'}); this is the old line
-  res.render('index.html');
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//  // res.render('index', { title: 'Express'}); this is the old line
+//   res.render('index.html');
+// });
 
-module.exports = router;
+// module.exports = router;
